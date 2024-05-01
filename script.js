629b9dafaa54cbc631c685e077486a021077842a
PshenAI <antonvoloshyn25@gmail.com>
2024-05-01 16:49:51 +0300

    Add logic for human choice

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3)
 
@@ -10,4 +14,29 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+
+    let toggle = true;
+
+    while(toggle) {
+        const userInput = prompt('Enter your choice: ').toLowerCase();
+
+        if(userInput === ROCK) {
+            toggle = false;
+            return "rock";
+        } else if (userInput === PAPER) {
+            toggle = false;
+            return "paper";
+        } else if (userInput === SCISSORS) {
+            toggle = false;
+            return "scissors";
+        } else {
+            console.log("Enter valid value! (rock | paper | scissors)");
+        }
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(getHumanChoice());
